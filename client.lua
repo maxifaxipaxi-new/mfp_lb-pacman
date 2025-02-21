64d31f7fd2839e5d6ae3b8369a48137ea631bf6a
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-02-21 07:48:02 +0100

    Removed Fivemanage Images
    Removed all Fivemanage Images that are not working anymore.

--- client.lua
+++ client.lua
@@ -14,7 +14,7 @@ CreateThread(function ()
             defaultApp = Config.defaultApp, 
             size = Config.size, 
             price = Config.price, 
-            images = {"https://r2.fivemanage.com/pub/osorkt341327.png", "https://r2.fivemanage.com/pub/3ejzs6uf2wh7.png", "https://r2.fivemanage.com/pub/qm41dvodhhjd.png"}, -- OPTIONAL array of images for the app on the app store
+            images = {}, -- OPTIONAL array of images for the app on the app store
             ui = GetCurrentResourceName() .. "/ui/index.html", 
             icon = "https://cfx-nui-" .. GetCurrentResourceName() .. "/ui/assets/icon.webp"
         })
